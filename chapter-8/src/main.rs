1d14d9afaacc8057290088cfab60967f76bc9531
Paul Mucur <mudge@mudge.name>
2017-05-16 22:04:27 +0100

    Add tyarith binary for typechecking STDIN

---
+++ chapter-8/src/main.rs
@@ -0,0 +1,26 @@
+extern crate tyarith;
+
+use std::io::{self, Read};
+use tyarith::{parse, type_of};
+
+fn main() {
+    let mut buffer = String::new();
+    match io::stdin().read_to_string(&mut buffer) {
+        Ok(_) => {
+            let program = parse(&buffer);
+
+            match program {
+                Ok(term) => {
+                    println!("Source program:    {}", term);
+
+                    match type_of(&term) {
+                        Ok(t) => println!("Type of program:   {}", t),
+                        Err(error) => println!("Type error:        {}", error),
+                    }
+                }
+                Err(e) => println!("Parse error:        {}", e),
+            }
+        },
+        _ => println!("Could not read from STDIN!"),
+    }
+}
